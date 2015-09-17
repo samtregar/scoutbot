72050efc68cccdd2b0b06c3f9601ea789dd31c63
Sam Tregar <sam@tregar.com>
2015-09-17 09:22:30 -0700

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+# Bootstrap installation of Distribute
+import distribute_setup
+distribute_setup.use_setuptools()
+
+import os
+
+from setuptools import setup
+
+
+PROJECT = u'ScoutBot'
+VERSION = '0.1'
+URL = ''
+AUTHOR = u'Sam Tregar'
+AUTHOR_EMAIL = u'sam@wawd.com'
+DESC = "A short description..."
+
+def read_file(file_name):
+    file_path = os.path.join(
+        os.path.dirname(__file__),
+        file_name
+        )
+    return open(file_path).read()
+
+setup(
+    name=PROJECT,
+    version=VERSION,
+    description=DESC,
+    long_description=read_file('README.rst'),
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    url=URL,
+    license=read_file('LICENSE'),
+    namespace_packages=[],
+    packages=[u'ScoutBot'],
+    package_dir = {'': os.path.dirname(__file__)},
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        # -*- Requirements -*-
+    ],
+    entry_points = {
+        # -*- Entry points -*-
+    },
+    classifiers=[
+    	# see http://pypi.python.org/pypi?:action=list_classifiers
+        # -*- Classifiers -*- 
+        'License :: OSI Approved',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        "Programming Language :: Python",
+    ],
+)
